0791120bd0ebe2a5397085b80db2de7865e26700
numan947 <runtime947@gmail.com>
2016-11-17 14:22:45 +0600

    CSE310 offline 3: Parser started

---
+++ CSE 310/offline 3/1305043/numan947.sh
@@ -0,0 +1,17 @@
+if bison -d -y 1305043.y; then
+	echo 'bison xxx.y DONE'
+	if g++ -w -c -o y.o y.tab.c; then
+		echo 'g++ y.tab.c DONE'
+		if flex 1305043.l; then
+			echo 'flex xxx.l  DONE'
+			if g++ -w -c -o l.o lex.yy.c; then
+			# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
+				echo 'g++ lex.yy.c DONE'
+				if g++ -o a.out y.o l.o -lfl -ly; then
+					echo './a.out DONE'
+					./a.out
+				fi
+			fi
+		fi
+	fi
+fi
