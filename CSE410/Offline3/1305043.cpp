e3145176455cb3a94a49f18b8f808e3cc142ed15
numan947 <mahmudulhasan947@gmail.com>
2018-06-17 17:45:24 +0600

    Continuing OFFLINE3, CSE410, ALGORITHM:1

--- CSE410/Offline3/1305043.cpp
+++ CSE410/Offline3/1305043.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "bitmap_image.hpp"
 using namespace std;
 
 
@@ -10,6 +11,7 @@ using namespace std;
 #define pi (2*acos(0.0))
 
 const int INF = (int) 1e9;
+const double eps = 10e-6;
 
 
 inline void tokenize(string str,vector<string> &tokens, string delim){ tokens.clear();size_t s = str.find_first_not_of(delim), e=s; while(s!=std::string::npos){e=str.find(delim,s);tokens.push_back(str.substr(s,e-s));s=str.find_first_not_of(delim,e);}}
@@ -32,7 +34,7 @@ double stringToDouble(string s)
 string doubleToString(double dd)
 {
 	ostringstream ostr;
-	ostr.precision(7);
+	ostr.precision(6);
 	ostr<<fixed;
 	ostr<<dd;
 	return ostr.str();
@@ -58,10 +60,8 @@ public:
 	}
 };
 
-
-
 class Triangle{
-
+private:
 	Point* tokenizeAndAdd(string in){
 
 		tokens.clear();
@@ -100,6 +100,23 @@ public:
 	}
 
 
+	double getMinY()
+	{
+		double mm = INF;
+		for(int i=0;i<3;i++)
+			mm = min(mm,point[i]->y);
+		return mm;
+	}
+
+	double getMaxY()
+	{
+		double mm = -INF;
+		for(int i=0;i<3;i++)
+			mm = max(mm,point[i]->y);
+		return mm;
+	}
+
+
 
 	void printTriangle()
 	{
@@ -107,22 +124,31 @@ public:
 			point[i]->printPoint();
 		printf("\n");
 	}
+
+	~Triangle()
+	{
+		for(int i=0;i<3;i++)
+			delete point[i];
+	}
 };
 
 
+double Screen_Width,
+	   Screen_Height,
+	   x_left_limit,
+	   x_right_limit,
+	   y_bottom_limit,
+	   y_top_limit,
+	   z_front_limit,
+	   z_rear_limit,
+	   Top_Y,
+	   Left_X,
+	   dx,
+	   dy;
 
+double **z_buffer;
 
-
-
-
-
-
-
-
-
-
-
-double Screen_Width,Screen_Height,x_limit,y_limit,z_limit_f,z_limit_r;
+bitmap_image *image;
 
 vector<Triangle*>triangles;
 
@@ -145,18 +171,20 @@ void read_data()
 
 	//2nd line
 	getline(if1,tmp);
-	x_limit = stringToDouble(tmp);
+	x_left_limit = stringToDouble(tmp);
+	x_right_limit = -x_left_limit;
 
 	//3rd line
 	getline(if1,tmp);
-	y_limit = stringToDouble(tmp);
+	y_bottom_limit = stringToDouble(tmp);
+	y_top_limit = -y_bottom_limit;
 
 	//4th line
 	getline(if1,tmp);
 	tokens.clear();
 	tokenize(tmp,tokens," ");
-	z_limit_f = stringToDouble(tokens[0]);
-	z_limit_r = stringToDouble(tokens[1]);
+	z_front_limit = stringToDouble(tokens[0]);
+	z_rear_limit = stringToDouble(tokens[1]);
 
 
 	string v2;
@@ -177,7 +205,7 @@ void read_data()
 	//validate
 
 /*	cout<<Screen_Width<<" X "<<Screen_Height<<endl;
-	cout<<x_limit<<" "<<y_limit<<" "<<z_limit_f<<" "<<z_limit_r<<endl;
+	cout<<x_left_limit<<" "<<y_bottom_limit<<" "<<z_front_limit<<" "<<z_rear_limit<<endl;
 
 	for(int i=0;i<triangles.size();i++)
 		triangles[i]->printTriangle();*/
@@ -187,17 +215,129 @@ void read_data()
 
 void initialize_z_buffer_and_frame_buffer()
 {
+	
+	//dx, dy
+	dx = (x_right_limit - x_left_limit)/Screen_Width;
+	dy = (y_top_limit - y_bottom_limit)/Screen_Height;
+
+	//top, left
+	Top_Y = 1-(dy/2.0);
+	Left_X = -1+(dx/2.0);
+
+	//z_buffer
+	z_buffer = new double*[(int)Screen_Height];
+	for(int i=0;i<Screen_Height;i++){
+		z_buffer[i] = new double[(int)Screen_Width];
+		for(int j=0;j<Screen_Width;j++){
+			z_buffer[i][j]=z_rear_limit;
+			//printf("%lf ",z_buffer[i][j]);
+		}
+		//printf("\n");
+	}
+
+	//image
+	image = new bitmap_image(Screen_Width,Screen_Height);
+    for(int i=0;i<Screen_Width;i++){
+        for(int j=0;j<Screen_Height;j++){
+            image->set_pixel(i,j,0,0,0);
+        }
+    }
+
+    //todo : REMOVE
+    image->save_image("test.bmp");
+}
+
+
+
+
+
+
+double getTopScanLine(double maxY)
+{
+	double ss = maxY;
+	if(ss>y_top_limit)ss=y_top_limit;
+	double to_ret = (Top_Y - ss)/dy;
+	cout<<to_ret<<endl;
+	return to_ret;
+}
+int getBottomScanline(double minY)
+{
+	double ss = minY;
+	if(ss<y_bottom_limit)ss=y_bottom_limit;
+	double to_ret = (Top_Y - ss)/dy;
+	cout<<to_ret<<endl;
+	return to_ret;
+}
+void apply_procedure()
+{
+	for(int i=0;i<triangles.size();i++)
+	{
+		Triangle* cur = triangles[i];
+
+		double top_scan_line = getTopScanLine(cur->getMaxY());
+		double bottom_scan_line = getBottomScanline(cur->getMinY());
+
+		double pp = top_scan_line;
+
+		while(pp<=bottom_scan_line){
+			pp+=dy;
+			
+		}
+		
+		printf("%lf\n",pp );
+
+
+
+
+	}
+}
+
+
+void save()
+{
+	image->save_image("1.bmp");
+
+
+	ofstream ofs;
+	ofs.open("z_buffer.txt");
+
+	for(int r=0;r<Screen_Height;r++){
+		for(int c=0;c<Screen_Width;c++){
+			if(z_buffer[r][c]<z_rear_limit)
+				ofs<<doubleToString(z_buffer[r][c])<<"\t";
+		}
+		ofs<<"\n";
+	}
+	ofs.close();
 
 }
 
+void free_memory()
+{
+	//free objects memory
+	for(vector<Triangle*>::iterator it = triangles.begin();it!=triangles.end();it++)
+		delete *it;
+	triangles.clear();
+	
+	//free image memeory
+	delete image;
+
+	//free z_buffer memory
+	for(int i=0;i<Screen_Height;i++){
+		delete[] z_buffer[i];
+	}
+	delete z_buffer;
+}
 
 
 int main()
 {
 
 	read_data();
-
-
+	initialize_z_buffer_and_frame_buffer();
+	apply_procedure();
+	//save();
+	free_memory();
 
 	return 0;
 }
--- CSE410/Offline3/Assignment 3/Docs/codes/image_drawing.cpp
+++ CSE410/Offline3/Assignment 3/Docs/codes/image_drawing.cpp
@@ -7,8 +7,8 @@ int main(){
 
     bitmap_image image(500,300);
 
-    for(int i=0;i<200;i++){
-        for(int j=0;j<100;j++){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<300;j++){
             image.set_pixel(i,j,255,255,0);
         }
     }
