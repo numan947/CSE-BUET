e217371648a718c7108d148c2747e660ce887189
numan947 <mahmudulhasan947@gmail.com>
2018-06-20 16:10:03 +0600

    fixed Random and Created Polygon Table and EdgeTable

--- CSE410/Offline3/1305043_scan_line.cpp
+++ CSE410/Offline3/1305043_scan_line.cpp
@@ -103,6 +103,32 @@ public:
 
 };
 
+class Vect
+{
+public:
+	double x,y,z;
+
+	Vect(Point p1,Point p2)
+	{
+		this->x = p2.x - p1.x;
+		this->y = p2.y - p1.y;
+		this->z = p2.z - p1.z;
+	}
+
+	Vect(double x, double y, double z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+};
+
+Vect crossProduct(Vect a,Vect b)
+{
+	Vect tmp(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+ 	return tmp;
+}
 
 
 
@@ -126,9 +152,12 @@ private:
 	}
 public:
 	Point *point[3];
+	bool inOutFlag;
+	double p_a,p_b,p_c,p_d;
+	int id;
 
 
-	Triangle(vector<string>vs)
+	Triangle(vector<string>vs,int id)
 	{
 		if(vs.size()>3){
 			printf("Error in --> TRIANGLE:init, too many vertices\n");
@@ -136,6 +165,19 @@ public:
 		else{
 			for(int i=0;i<3;i++)
 				point[i]=tokenizeAndAdd(vs[i]);
+			
+
+			Vect *A = new Vect(*point[0],*point[1]);
+			Vect *B = new Vect(*point[0],*point[2]);
+
+			Vect planeVector = crossProduct(*A,*B);
+
+			this->p_a = planeVector.x;
+			this->p_b = planeVector.y;
+			this->p_c = planeVector.z;
+			this->p_d = (-p_a*(point[0]->x))+(-p_b*point[0]->y)+(-p_c*point[0]->z);
+			this->inOutFlag = false;
+			this->id = id;
 		}
 
 	}
@@ -164,21 +206,106 @@ public:
 		printf("\n");
 	}
 
+	void printPolygon()
+	{
+		printf("%d|%lfX+%lfY+%lfZ+%lf|(%d,%d,%d)|%d",id,p_a,p_b,p_c,p_d,color[0],color[1],color[2],inOutFlag);
+	}
+
 	~Triangle()
 	{
 		for(int i=0;i<3;i++){
 			delete point[i];
-			delete line[i];
+			
 		}
 	}
 };
 
+
+
 class EdgeBucket{
 public:
-	double yMax,yMin,x,sign,dX,dY,sum;
+	double yMax,x,dX;
+	int id,tableIdx;
+
+	EdgeBucket(int tableIdx,double yMax,double x, double dX, int id)
+	{
+		this->tableIdx = tableIdx;
+		this->yMax = yMax;
+		this->x = x;
+		this->dX = dX;
+		this->id = id;
+	}
+
+    void printEdgeBucket()
+    {
+    	cout<<"tableIdx: "<<tableIdx<<endl;
+    	cout<<"id: "<<id<<endl;
+    	cout<<"yMax: "<<yMax<<endl;
+    	cout<<"x(at yMin): "<<x<<endl;
+    	cout<<"dX: "<<dX<<endl;
+    }
+
+    void printEdgeBucket2()
+    {
+    	cout<<yMax<<"|"<<x<<"|"<<dX<<"|"<<id;
+    }
 };
 
 
+
+bool comparePtrToNode(EdgeBucket* a, EdgeBucket* b) 
+{ 
+	return (fabs(a->x - b->x)<=eps || a->x< b->x); 
+}
+
+
+class EdgeTableEntry{
+public:
+	vector<EdgeBucket*>edgeBucketList;
+
+	EdgeTableEntry(){
+
+	}
+
+	void addEdgeBucket(EdgeBucket* edgeBucket)
+	{
+		edgeBucketList.pb(edgeBucket);
+	}
+
+	void sortEdgeBucketList()
+	{
+		sort(edgeBucketList.begin(),edgeBucketList.end(),comparePtrToNode);
+	}
+
+	void printEdgeTableEntry()
+	{
+		if(edgeBucketList.size()){
+			cout<<"|";
+			edgeBucketList[0]->printEdgeBucket2();
+			cout<<"|--->";
+			for(int i=1;i<edgeBucketList.size();i++)
+			{
+				cout<<"|";
+				edgeBucketList[i]->printEdgeBucket2();
+				cout<<"|--->";
+			}
+			cout<<"END"<<endl;
+		}
+	}
+
+	bool isEmpty()
+	{
+		return edgeBucketList.size()==0;
+	}
+
+	~EdgeTableEntry(){
+		for(vector<EdgeBucket*>::iterator it = edgeBucketList.begin();it!=edgeBucketList.end();it++)
+			delete *it;
+		edgeBucketList.clear();
+	}
+
+
+};
 
 double Screen_Width,
 	   Screen_Height,
@@ -198,7 +325,8 @@ bitmap_image *image;
 vector<Triangle*>triangles;
 
 
-EdgeBucket** edgeTable;
+EdgeTableEntry *edgeTable;
+
 
 
 
@@ -240,13 +368,14 @@ void read_data()
 	string v3;
 	vector<string>vs;
 	//parsing stage3.txt
+	int ids = 0;
 	while(getline(if2,tmp)){
 		if(tmp.size()>=5){
 			getline(if2,v2);
 			getline(if2,v3);
 			vs.clear();
 			vs.pb(tmp);vs.pb(v2);vs.pb(v3);
-			triangles.pb(new Triangle(vs));
+			triangles.pb(new Triangle(vs,ids++));
 		}
 	}
 
@@ -261,8 +390,106 @@ void read_data()
 }
 
 
+int getDiscreteY(double yy)
+{
+	return myRound((yy-Bottom_Y)/dy);
+}
+
+
+EdgeBucket* makeEdgeBucket(Point p1, Point p2,int id)
+{
+	EdgeBucket* ret;
+
+	double dX = p1.x - p2.x;
+	double dY = p1.y - p2.y;	
+
+	if(dX*dY<0)dX = -1.0 * fabs(dX);
+
+	if(fabs(p1.y - p2.y)<=eps || p1.y < p2.y){ //p1 has minY, p2 has maxY
+		int tdx = getDiscreteY(p1.y);
+		ret = new EdgeBucket(tdx,p2.y,p1.x,dX,id);
+	}
+	else{
+		int tdx = getDiscreteY(p2.y);
+		ret = new EdgeBucket(tdx,p1.y,p2.x,dX,id);
+	}
+
+	return ret;
+}
+
 void initialize_edge_table_and_polygon_table()
 {
+	//dx, dy
+	dx = (x_right_limit - x_left_limit)/Screen_Width;
+	dy = (y_top_limit - y_bottom_limit)/Screen_Height;
+
+	//bottom, left
+	Bottom_Y = y_bottom_limit+(dy/2.0);
+	Left_X = x_left_limit+(dx/2.0);
+
+	printf("Bottom_Y %lf Left_X %lf\n",Bottom_Y,Left_X);
+	printf("dx %lf dy %lf\n",dx,dy);
+
+
+	edgeTable = new EdgeTableEntry[myRound(Screen_Width)];
+
+	Triangle* cur;
+
+	for(int i=0;i<triangles.size();i++)
+	{
+		cur = triangles[i];
+		cur->setColor(myRand(),myRand(),myRand());
+
+		EdgeBucket* e1 = makeEdgeBucket(*(cur->point[0]),*(cur->point[1]),cur->id);
+		EdgeBucket* e2 = makeEdgeBucket(*(cur->point[0]),*(cur->point[2]),cur->id);
+		EdgeBucket* e3 = makeEdgeBucket(*(cur->point[1]),*(cur->point[2]),cur->id);
+
+		edgeTable[e1->tableIdx].addEdgeBucket(e1);
+		edgeTable[e2->tableIdx].addEdgeBucket(e2);
+		edgeTable[e3->tableIdx].addEdgeBucket(e3);
+/*
+		e1->printEdgeBucket();
+
+		cout<<endl;
+
+		e2->printEdgeBucket();
+
+		cout<<endl;
+
+		e3->printEdgeBucket();
+		cout<<endl;
+*/
+
+	}
+
+
+	printf("EDGE TABLE\n\n");
+	for(int i=0;i<Screen_Height;i++)
+	{
+		if(!edgeTable[i].isEmpty()){
+			edgeTable[i].sortEdgeBucketList();
+			cout<<i<<"--->";
+			edgeTable[i].printEdgeTableEntry();
+		}
+	}
+
+	printf("\n\n");
+
+	printf("POLYGON TABLE\n\n");
+
+	for(int i=0;i<triangles.size();i++)
+	{
+		triangles[i]->printPolygon();
+		cout<<endl;
+	}
+
+
+
+
+
+
+
+
 
 }
 
@@ -302,9 +529,9 @@ void free_memory()
 
 int main()
 {
-
+	//srand(time(NULL));
 	read_data();
-
+	initialize_edge_table_and_polygon_table();
 	//save();
 	free_memory();
 
