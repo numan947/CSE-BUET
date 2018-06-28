5cca71d30be3425d7205640de25ae167c6a211aa
numan947 <mahmudulhasan947@gmail.com>
2018-06-28 16:07:13 +0600

    Half COmpleted offline3 cse472

--- CSE472/Offline3/1305043 code.py
+++ CSE472/Offline3/1305043 code.py
@@ -37,39 +37,93 @@ def make_dummy():
 	RATE_LOW = 0
 	RATE_HIGH = 5
 
-	ratings = np.zeros((MAXUSER, MAXITEM))
-
-	for i in range(MAXUSER):
-		for j in range(MAXITEM):
-			ratings[i][j] = np.random.randint(RATE_LOW, RATE_HIGH)
-
+	ratings = np.random.randint(RATE_LOW,RATE_HIGH,(MAXUSER, MAXITEM))
+	print(ratings)
 	sparsity = 100.0*float(np.count_nonzero(ratings)/(ratings.shape[0]*ratings.shape[1]))
 
+
+
 	print("sparsity:",sparsity)
-	print(ratings)
 	print("\nDATA READING COMPLETED.......................\n")
 	return ratings,sparsity
 
 
-
-
-
 def read_data(col_names):
-	
-	
-
-
-
-	
-	
 	return make_dummy()
 
+def train_validation_test_split(ratings):
+	train,test = train_test_split(ratings,test_size=0.4,random_state=94743)
+	validation,test = train_test_split(test,test_size=0.5,random_state=94743)
+	print(len(train))
+	print(len(validation))
+	print(len(test))
+	return train,validation,test
+
+def calculate_rmse(pred,actual):
+	pred=pred[actual.nonzero()].flatten()
+	actual=actual[actual.nonzero()].flatten()
+	return np.sqrt(mean_squared_error(pred,actual))
 
 
 
+class Matrix_Factorization():
+
+	def __init__(self,ratingMatrix,latentFactors=5,lambdaU=0,lambdaV=0,printIntermediate=False):
+		self.u_len,self.v_len = ratingMatrix.shape
+		self.ratingMatrix = ratingMatrix
+		self.latentFactors = latentFactors
+		self.lambdaU = lambdaU
+		self.lambdaV = lambdaV
+		self.pr = printIntermediate
 
 
+	def ALSU(self,U,V,X,lam):
+		VTV = V.T.dot(V)
+		lamI = np.eye(VTV.shape[0])*lam
 
+		#print(X[0,:].shape)
+
+		#print(V.shape)
+
+		for p in range(U.shape[0]):
+			pass
+			U[p,:] = solve((VTV+lamI), X[p,:].T.dot(V))
+		return U
+
+	def ALSV(self,U,V,X,lam):
+		UTU = U.T.dot(U)
+		lamI = np.eye(UTU.shape[0])*lam
+		for p in range(V.shape[0]):
+			V[p,:] = solve(UTU+lamI, X[:,p].T.dot(U))
+		return V
+
+	def Train(self,iter = 20):
+
+		self.U = np.random.random((self.u_len,self.latentFactors))
+		self.V = np.random.random((self.v_len,self.latentFactors))
+
+
+		cur = 0
+
+		while(cur<iter):
+			if self.pr==True and cur%5==4:
+				print("------------->Iteration Number....",cur+1)
+			
+			self.U = self.ALSU(self.U, self.V, self.ratingMatrix, self.lambdaU)
+			self.V = self.ALSV(self.U, self.V, self.ratingMatrix, self.lambdaV)
+
+			cur+=1
+
+
+	def Predict_one(self,u,v):
+		return self.U[u,:].dot(self.V[v,:].T)
+
+	def Predict(self):
+		predictions = np.zeros((self.u_len,self.v_len))
+		for u in range(self.u_len):
+			for v in range(self.v_len):
+				predictions[u,v] = self.Predict_one(u,v)
+		return predictions
 
 
 
@@ -99,6 +153,21 @@ def main():
 
 	col_names = ['UID','IID','RTN','TMS']
 	ratings,sparsity = read_data(col_names)
+
+	train,validation,test = train_validation_test_split(ratings)
+
+
+	print(train)
+
+	MF = Matrix_Factorization(train,latentFactors=7,printIntermediate=True)
+
+	MF.Train()
+
+	print(MF.Predict())
+
+
+	print(calculate_rmse(MF.Predict(),train))
+
 
 
 
